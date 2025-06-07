c81d1186b24559f0d8374e54f03b6a145b38ed7c
Dawid Pawliczek <dawidpawliczek4@gmail.com>
2025-06-07 15:49:37 +0200

    change downloading script

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-curl -L -o ./downloaded\
+curl -L -o ./downloaded_data.zip\
   https://www.kaggle.com/api/v1/datasets/download/astraszab/facial-expression-dataset-image-folders-fer2013
+
+unzip -o downloaded_data.zip -d ./downloaded_data
+rm downloaded_data.zip
