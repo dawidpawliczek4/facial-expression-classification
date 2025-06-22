173dec906c100d2033e48ff4d16b3de0724eca8a
MateuszGar <mgarncarczyk@onet.eu>
2025-06-22 12:49:08 +0200

    Update ViT parameters

--- data/download.sh
+++ data/download.sh
@@ -2,5 +2,5 @@
 curl -L -o ./downloaded_data.zip\
   https://www.kaggle.com/api/v1/datasets/download/astraszab/facial-expression-dataset-image-folders-fer2013
 
-unzip -o downloaded_data.zip -d ./downloaded_data
-rm downloaded_data.zip
+unzip -o downloaded_data.zip -d ./downloaded_data > /dev/null 2>&1
+rm downloaded_data.zip > /dev/null 2>&1
